408e175cb930f42f5801ed13f29c7fa60cbe8e31
unknown <fikrinizall@gmail.com>
2023-01-29 21:08:27 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const navbar = document.querySelector('.header .nav');
+const menuButton = document.querySelector('.header .menu');
+
+
+document.onscroll = () => {
+
+  if (window.scrollY > 0) {
+    document.querySelector('.header').classList.add('active');
+  } else {
+    document.querySelector('.header').classList.remove('active');
+  }
+};
+
+document.onload = () => {
+  if (window.scrollY > 0) {
+    document.querySelector('.header').classList.add('active');
+  } else {
+    document.querySelector('.header').classList.remove('active');
+  }
+};
+
+
